37d51221e0a09030889a4f031f65dfbd4653a7fd
freedom_lovver <maksbuharskij16@gmail.com>
2025-09-07 17:03:53 +0500

    feat: representing a user with id, username, and age. Mapped to 'users' table in the database.

---
+++ spring-boot-learning/src/main/java/SpringBoot4/Sorting/Users/AppUser.java
@@ -0,0 +1,48 @@
+package SpringBoot4.Sorting.Users;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private Integer age;
+
+    public AppUser() {}
+
+    public AppUser(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "User [ id=" + id + ", name='" + username + "', age=" + age + ']';
+    }
+
+}
