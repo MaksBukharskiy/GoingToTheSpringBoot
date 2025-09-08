377dd3791303999b26406f4b11a37c616e1d10d8
freedom_lovver <maksbuharskij16@gmail.com>
2025-09-08 22:49:48 +0500

    feat: simple entity User, added some cells, configured output

---
+++ spring-boot-learning/src/main/java/SpringBoot5/RestApiStarter/AppUsers/User.java
@@ -0,0 +1,59 @@
+package SpringBoot5.RestApiStarter.AppUsers;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String car;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String car, Integer age) {
+        this.car = car;
+        this.age = age;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCar() {
+        return car;
+    }
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User {" +
+                "id=" + id +
+                ", car='" + car + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
+
