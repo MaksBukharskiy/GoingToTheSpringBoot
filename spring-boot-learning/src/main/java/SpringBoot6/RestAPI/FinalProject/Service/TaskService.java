afdda45052d5658f6fc4d0e51fac2510d7800977
freedom_lovver <maksbuharskij16@gmail.com>
2025-09-21 23:46:38 +0500

    feat: started to implement TaskService with business logic for CRUD operations

--- spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Service/TaskService.java
+++ spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Service/TaskService.java
@@ -1,4 +1,32 @@
 package SpringBoot6.RestAPI.FinalProject.Service;
 
+import SpringBoot6.RestAPI.FinalProject.Entity.Task;
+import SpringBoot6.RestAPI.FinalProject.Repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks(String status) {
+        if((status != null) && !status.isEmpty()){
+            try{
+                Task.Status enumStatus = Task.Status.valueOf(status.toUpperCase());
+                return taskRepository.findByStatus(enumStatus);
+            }
+            catch (IllegalArgumentException e){
+                return taskRepository.findAll();
+            }
+        }
+        return taskRepository.findAll();
+    }
+
+
+
 }
