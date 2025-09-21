ed723e4782b76fc2f89df198de0ba08916b57f33
freedom_lovver <maksbuharskij16@gmail.com>
2025-09-21 22:51:59 +0500

    feat: add TaskRepository interface extending JpaRepository

--- spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Repository/TaskRepository.java
+++ spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package SpringBoot6.RestAPI.FinalProject.Repository;
 
-public class TaskRepository {
+import SpringBoot6.RestAPI.FinalProject.Entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(Task.Status status);
 }
