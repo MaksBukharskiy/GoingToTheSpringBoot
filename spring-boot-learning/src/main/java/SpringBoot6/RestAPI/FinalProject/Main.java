f8d2f5fd0a0b6bdb044cfd0f1d8306fcdad7f62b
freedom_lovver <maksbuharskij16@gmail.com>
2025-09-23 14:05:15 +0500

    feat: added main

--- spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Main.java
+++ spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Main.java
@@ -1,4 +1,11 @@
-package SpringBoot6.RestAPI.FinalProject.Service;
+package SpringBoot6.RestAPI.FinalProject;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
