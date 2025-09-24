64d917f3c6fc418d6f940fa1781025d1eb24086d
freedom_lovver <maksbuharskij16@gmail.com>
2025-09-24 13:45:55 +0500

    update: add TaskController with GET, POST, PUT, DELETE endpoints

--- spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Controller/TaskController.java
+++ spring-boot-learning/src/main/java/SpringBoot6/RestAPI/FinalProject/Controller/TaskController.java
@@ -1,4 +1,61 @@
 package SpringBoot6.RestAPI.FinalProject.Controller;
 
+import SpringBoot6.RestAPI.FinalProject.Entity.Task;
+import SpringBoot6.RestAPI.FinalProject.Repository.TaskRepository;
+import SpringBoot6.RestAPI.FinalProject.Service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Task task = taskService.getTaskById(id);
+
+        if (task == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(task);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks(@RequestParam(required = false) String _status) {
+        List<Task> tasks = taskService.getAllTasksBy(_status);
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task taskCreated = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        Task updated = taskService.updateTask(task, id);
+
+        if (updated == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        boolean deleted = taskService.deleteTaskById(id);
+        if (!deleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
